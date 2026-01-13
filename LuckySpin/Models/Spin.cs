8ff96e56f1bf9baa08e5b7f2c8121f086a8a3ee0
Patrick <234474225+Gliese-581c@users.noreply.github.com>
2026-01-13 14:16:04 -0800

    removed extra bracer

--- LuckySpin/Models/Spin.cs
+++ LuckySpin/Models/Spin.cs
@@ -8,4 +8,3 @@ public class Spin
     public required int[] Numbers { get; set; } //Made required to ensure it's always set or an error is thrown
     public string ImageDisplay { get; set; } = "none"; //Default to "none" so the image is hidden unless set to "block"
 }
-}
